97bb2b5d666a5b2b7cbd3a7a614b46100af02053
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-26 11:28:23 +0530

    main func

---
+++ project-02-jwt-login/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var router *mux.Router = mux.NewRouter()
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("GET")
+
+	log.Println("Listen on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("signup invoked")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("login invoked")
+
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("protectedEndpoint invoked")
+
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	fmt.Println("TokenVerifyMiddleware invoked")
+	return nil
+}
